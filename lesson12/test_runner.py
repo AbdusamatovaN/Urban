c94e53f577604fe974047c1afddacd20f3fd29a8
AbdusamatovaN <niginaabdusamatova616@gmail.com>
2024-09-18 20:15:32 +0300

    urban

---
+++ lesson12/test_runner.py
@@ -0,0 +1,27 @@
+import runner
+import unittest
+
+from lesson12.runner import Runner
+
+
+class RunnerTest(unittest.TestCase):
+    def test_run(self):
+        runner = Runner("Rock")
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_walk(self):
+        runner = Runner("Rock")
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_challenge(self):
+        runner1 = Runner("Rock")
+        runner2 = Runner("Karl")
+        for i in range(10):
+            runner1.walk()
+            runner2.run()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
