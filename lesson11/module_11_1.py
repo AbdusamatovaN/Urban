f436cc841d474422fa2c26334a78eba533c78913
AbdusamatovaN <niginaabdusamatova616@gmail.com>
2024-11-01 21:47:54 +0300

    modified

--- lesson11/module_11_1.py
+++ lesson11/module_11_1.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 
 r = requests.get('https://api.github.com/events')
+print(r.status_code)
 print(r.text)
 
 s = pd.Series([1, 3, 5, np.nan, 6, 8])
+data = pd.DataFrame(s)
 print(s)
 
 a = np.array([[1, 2, 3],
