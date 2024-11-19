755e633b74f8d7cdb941f50b44eaf77bb14d2b28
AbdusamatovaN <niginaabdusamatova616@gmail.com>
2024-11-19 20:50:35 +0300

    modified

--- lesson14/crud_functions.py
+++ lesson14/crud_functions.py
@@ -1,23 +1,56 @@
 import sqlite3
 
-def get_all_products():
-    connection = sqlite3.connect("database.db")
-    cursor = connection.cursor()
-    cursor.execute('DELETE FROM Products')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Products(
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        description TEXT,
-        price INTEGER NOT NULL
-        )
-        ''')
 
-    for i in range(1, 5):
-        cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
-                       (f"Product {i}", f"Description {i}", i * 100))
-    cursor.execute('SELECT * FROM Products')
-    res = cursor.fetchall()
+def initiate_db():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    );
+    ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL DEFAULT 1000
+    );
+    ''')
     connection.commit()
     connection.close()
-    return res
+
+def get_all_products():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Products")
+    products = cursor.fetchall()
+    connection.commit()
+    return products
+
+def add_user(username, email, age):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    if not is_include(username):
+        cursor.execute("INSERT INTO Users (username, email, age) VALUES(?, ?, ?)",
+                       (f'{username}', f'{email}', f'{age}'))
+    connection.commit()
+    connection.close()
+
+def is_include(username):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    check_user = cursor.execute("SELECT * FROM Users WHERE username=?", (username,))
+    if check_user.fetchone() is None:
+        flag = False
+    else:
+        flag = True
+    connection.commit()
+    connection.close()
+    return flag
+
