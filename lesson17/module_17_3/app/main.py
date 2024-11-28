4d9de6183b8f4603b7ec72036ed9824d769ef444
AbdusamatovaN <niginaabdusamatova616@gmail.com>
2024-11-28 20:23:45 +0300

    modified

---
+++ lesson17/module_17_3/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routers.user import user
+from routers.task import task
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Welcome to Taskmanager"}
+
+app.include_router(user)
+app.include_router(task)
